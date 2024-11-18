fa4b024fb963d780d4d8f6483ba91eb3a316d7f6
Gerard <1751695@uab.cat>
2024-11-18 19:45:33 +0100

    primera càrrega

---
+++ pract3.c
@@ -0,0 +1,100 @@
+/* Exercici 1:
+int main(){
+int i,j;
+i = 1;
+j = 2;
+i++;
+j += i;
+return 0;
+}
+*/
+
+/* Exercici 2:
+int main(){
+short i,j;
+i = 1;
+j = 2;
+i++;
+j += i;
+return 0;
+}
+*/
+
+/* Exercici 3:
+int main(){
+   int a,b,c,d;
+   a = 1;
+   b = 2;
+   c = 3;
+   d = 4;
+   a = b+c*d;
+   a = (b+c)*d;
+   d = a;
+}*/
+
+/* Exercici 4.1:
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int var = 3;
+    char car = 'a';
+    int res;
+    res = var * car;
+    return 0;
+}*/
+
+/*Exercici 4.2:
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int var = 3;
+    char car = 'a';
+    char res;
+    res = var * car;
+    return 0;
+}
+*/
+
+/* Exercici 5.1:
+int main(){
+short a[3];
+a[0] = 1;
+a[1] = 2;
+a[2] = 3;
+return 0;
+}
+*/
+
+/* Exercici 5.2:
+int main(){
+char a[3];
+a[0] = 1;
+a[1] = 2;
+a[2] = 3;
+return 0;
+}
+*/
+
+/* Exercici 6:
+#include <stdlib.h>
+int main(){
+   int a[]={1,2,3,4};
+   int b []= {5,6,7,8};
+   for (int i=0; i<4; i++){
+   int c = a[i]*b[i];
+   }
+   return 0;
+}
+*/
+
+/* Exercici 7:
+int main(){
+ int i, a[20];
+ for( i = 0; i < 20; i++ ){
+ a[i] = i*4;
+ }
+ return 0;
+}
+*/
